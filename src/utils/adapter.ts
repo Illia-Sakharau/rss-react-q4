260fccbf7537d7d51c5eeb5fbfb5bd67b6503389
Illia Sakharau <sakharovia0204@gmail.com>
2023-10-30 23:37:50 +0400

    fix: adapter input data

--- src/utils/adapter.ts
+++ src/utils/adapter.ts
@@ -1,12 +1,15 @@
 import { Art, ResponseArtworkInfo } from '../types';
+import noImg from '../assets/no-img.svg';
 
 export default function (resp: ResponseArtworkInfo): Art {
-  const id = resp.id;
-  const title = resp.title;
-  const artist = resp.artist_display;
-  const date = resp.date_display;
-  const imgURL = `https://www.artic.edu/iiif/2/${resp.image_id}/full/400,/0/default.jpg`;
-  const imgAlt = resp.thumbnail.alt_text;
+  const id = Number(resp.id);
+  const title = `${resp.title}`;
+  const artist = `${resp.artist_display}`;
+  const date = `${resp.date_display}`;
+  const imgURL = resp.image_id
+    ? `https://www.artic.edu/iiif/2/${resp.image_id}/full/600,/0/default.jpg`
+    : noImg;
+  const imgAlt = resp.thumbnail?.alt_text || 'without pictures';
 
   return {
     id,
