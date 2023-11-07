a6b91efe5e308f05077c5085eaa681161d79a83e
Illia Sakharau <sakharovia0204@gmail.com>
2023-11-07 22:42:38 +0400

    fix: redirect for netlify deploy

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import classes from './style.module.scss';
 import { FC, ReactElement } from 'react';
 import { SectionHeader } from '../../components/atoms/headers/Headers';
+import { Link } from 'react-router-dom';
 
 type Props = unknown;
 
@@ -8,6 +9,7 @@ const Error: FC<Props> = (): ReactElement => {
   return (
     <div className={classes.page}>
       <SectionHeader>Home Page</SectionHeader>
+      <Link to={'/gallery'}>Gallery</Link>
     </div>
   );
 };
