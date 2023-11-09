7be7c634bfbe98ef2b6ddd5e32d7d36dc590446e
Illia Sakharau <sakharovia0204@gmail.com>
2023-11-09 23:40:54 +0400

    config: setup vitest

---
+++ tests/test.test.tsx
@@ -0,0 +1,15 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { SectionHeader } from '../src/components/atoms/headers/Headers';
+
+describe('Test describe', () => {
+  it('First it', () => {
+    const arr = [1, 2, 3];
+    expect(arr[1]).toEqual(2);
+  });
+
+  it('Second test', () => {
+    render(<SectionHeader>Test text</SectionHeader>);
+    expect(screen.getByRole('heading', { level: 3 })).toHaveTextContent('123');
+  });
+});
