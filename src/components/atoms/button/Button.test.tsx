4ae03f0004955f30f41781bcec642425e0a4c348
Illia Sakharau <sakharovia0204@gmail.com>
2023-11-10 18:24:20 +0400

    test: add tests for Button component

---
+++ src/components/atoms/button/Button.test.tsx
@@ -0,0 +1,32 @@
+import { describe, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Button from './Button';
+
+describe('Button', () => {
+  const testText = 'Test text';
+  let testData = '';
+
+  beforeEach(() => {
+    render(
+      <Button
+        onClick={() => {
+          testData = testText;
+        }}
+      >
+        {testText}
+      </Button>
+    );
+  });
+
+  it('Text correct', () => {
+    expect(screen.getByRole('button')).toHaveTextContent(testText);
+  });
+
+  it('onClick work', () => {
+    const buttonEl = screen.getByRole('button');
+
+    expect(testData).toBe('');
+    fireEvent.click(buttonEl);
+    expect(testData).toBe(testText);
+  });
+});
