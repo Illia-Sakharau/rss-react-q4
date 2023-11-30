60a72b953943c6f1eadf55c8251e48dc52c79379
Illia Sakharau <sakharovia0204@gmail.com>
2023-11-30 13:36:19 +0400

    config: uninstall test depandencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest" />
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
@@ -7,23 +6,4 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setupTest'],
-    css: true,
-    coverage: {
-      provider: 'v8',
-      all: true,
-      include: ['src/**/*.tsx', 'src/**/*.ts'],
-      exclude: [
-        'src/API',
-        'src/test',
-        'src/setupTest.ts',
-        'src/types.ts',
-        'src/vite-env.d.ts',
-        'src/main.tsx',
-      ],
-    },
-  },
 });
