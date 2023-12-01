24ca8276b9451df12402b56285b9fb7231da7c8f
Illia Sakharau <sakharovia0204@gmail.com>
2023-12-01 23:21:03 +0400

    feat: add textfullInput component

---
+++ src/components/1-atoms/inputs/Inputs.tsx
@@ -0,0 +1,45 @@
+import { FieldError, RefCallBack } from 'react-hook-form';
+import classes from './style.module.scss';
+import {
+  ChangeEvent,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  forwardRef,
+} from 'react';
+
+type Props = {
+  type: 'text' | 'number' | 'email' | 'password';
+  label: string;
+  error: FieldError | undefined;
+
+  name: string;
+  ref: RefCallBack;
+  onBlur: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+} & DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+
+export const TextfullInput = forwardRef<HTMLInputElement, Props>(
+  ({ type, label, error, name, onBlur, onChange, ...props }, ref) => {
+    const className = !error
+      ? classes['input-wrapper']
+      : `${classes['input-wrapper']} ${classes['input-wrapper_error']}`;
+
+    return (
+      <div className={className}>
+        <label htmlFor={label}>{label}</label>
+        <input
+          type={type}
+          id={label}
+          name={name}
+          onBlur={onBlur}
+          onChange={onChange}
+          {...props}
+          ref={ref}
+        />
+        <div className={classes['error']}>
+          {error && <p>{error.message}</p>}
+        </div>
+      </div>
+    );
+  }
+);
