a37d4a55de6ee4f33ac2e5b7f6fa5b5a18990cf9
Illia Sakharau <sakharovia0204@gmail.com>
2023-12-01 23:35:44 +0400

    fix: 404 page

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -1,6 +1,7 @@
 import classes from './style.module.scss';
 import { FC, ReactElement } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '../../components/1-atoms/button/Button';
 
 type Props = unknown;
 
@@ -13,13 +14,13 @@ const NotFound: FC<Props> = (): ReactElement => {
         <div className={classes.code}>404</div>
         <div className={classes.text}>Page Not Found</div>
       </div>
-      <button
+      <Button
         onClick={() => {
           navigate('/');
         }}
       >
         Go Home
-      </button>
+      </Button>
     </div>
   );
 };
