fb006fd71c527cd23cecec3c8a6a47e4624d7c3b
Illia Sakharau <sakharovia0204@gmail.com>
2023-12-03 00:12:08 +0400

    feat: add checkbox component

---
+++ src/components/1-atoms/checkbox/checkbox.tsx
@@ -0,0 +1,56 @@
+import { FieldError, RefCallBack } from 'react-hook-form';
+import classes from './style.module.scss';
+import {
+  ChangeEvent,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  forwardRef,
+} from 'react';
+
+type Props = {
+  label: string;
+  error: FieldError | undefined;
+  required?: boolean;
+  name: string;
+  ref: RefCallBack;
+  onBlur: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+} & DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+
+const Checkbox = forwardRef<HTMLInputElement, Props>(
+  (
+    {
+      label,
+      error,
+      // required,
+      name,
+      onBlur,
+      onChange,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <div className={classes['input-wrapper']}>
+        <label className={classes.container} htmlFor={label}>
+          <input
+            type={'checkbox'}
+            id={label}
+            name={name}
+            onBlur={onBlur}
+            onChange={onChange}
+            {...props}
+            ref={ref}
+          />
+          <span className={classes.checkmark}></span>
+          {label}
+        </label>
+        <div className={classes['error']}>
+          {error && <p>{error.message}</p>}
+        </div>
+      </div>
+    );
+  }
+);
+
+export default Checkbox;
